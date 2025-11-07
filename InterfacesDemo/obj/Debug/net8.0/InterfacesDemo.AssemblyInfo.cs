ad297109ddccbed38931fb0f708622cfc2a4b58b
Ira <ira350520@gmail.com>
2025-11-07 10:37:12 +0200

    Add .gitignore only

--- InterfacesDemo/obj/Debug/net8.0/InterfacesDemo.AssemblyInfo.cs
+++ InterfacesDemo/obj/Debug/net8.0/InterfacesDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterfacesDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e957586026dca9d4940df800b5731c6696a6991")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterfacesDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterfacesDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
